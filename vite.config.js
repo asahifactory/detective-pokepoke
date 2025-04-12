5bbcbc76fadb1ba43c50380aea07d81ef4da127e
asahi <kudobat@gmail.com>
2025-04-12 12:47:12 +0900

    minor update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/detective-pokepoke/', // or whatever your repo name is
   plugins: [react()],
 })
